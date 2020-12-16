fee363a16e32af32237dd442f8163b7ac3e53ce0
Zulaas <jan@ueberruhr.de>
2020-12-16 12:10:23 +0100

    Average calculation and write everthing in csv output

--- sample/sample.py
+++ sample/sample.py
@@ -2,7 +2,6 @@ import datetime
 import pandas as pd
 
 
-# Todo what is with idx equals 0 but the value is bigger than the search value ?
 def findClosestIndex(search, df):
     idx = (df['Time'] - search).abs().idxmin()
     values = df.loc[idx]
@@ -29,55 +28,68 @@ def calculateEndTime(endToTest, absolutEnd, absolutStart):
 
 
 def countTimeDriven(start, rowEnd, df):
-    absolut = start
     startIdx = findClosestIndex(start, df)
     values = df.loc[startIdx]
     end = calculateEndTime(values['Time'], rowEnd, start)
     timeDriven = datetime.timedelta(0)
+    maxIdx = len(df.index)
     while start < rowEnd:
         if values['Value'] == 1:
             timeDriven += relativeDelta(start, end)
         start = end
         startIdx += 1
-        # Todo idx should not be bigger than the end index in the dataframe
+        if startIdx > maxIdx:
+            raise Exception(f'IndexOutOf BoundException startIdx:{startIdx} than the max index of the df:{maxIdx}')
         values = df.loc[startIdx]
-        end = calculateEndTime(values['Time'], rowEnd, absolut)
+        end = calculateEndTime(values['Time'], rowEnd, start)
     return timeDriven
 
 
 def getCycles():
-    cycles = pd.read_csv("data/cycle_intervals_complete_2019_11.csv")
+    cycles = pd.read_csv("data/input/cycle_intervals_complete_2019_11.csv")
     cycles['Begin'] = pd.to_datetime(cycles['Begin'])
     cycles['End'] = pd.to_datetime(cycles['End'])
     return cycles
 
 
 def getSensorBackward():
-    backward = pd.read_csv("data/sensor_FW_Rückwärts_complete_2019_11.csv")
+    backward = pd.read_csv("data/input/sensor_FW_Rückwärts_complete_2019_11.csv")
     backward['Time'] = pd.to_datetime(backward['Time'])
     return backward
 
 
 def getSensorForward():
-    forward = pd.read_csv("data/sensor_FW_Vorwärts_complete_2019_11.csv")
+    forward = pd.read_csv("data/input/sensor_FW_Vorwärts_complete_2019_11.csv")
     forward['Time'] = pd.to_datetime(forward['Time'])
     return forward
 
 
 def analyzeCycles(cycles, backward, forward):
+    df = pd.DataFrame(columns=['timeDrivenBackward', 'timeDrivenForward'])
     for index, cycle in cycles.iterrows():
         timeDrivenBackward = countTimeDriven(cycle['Begin'], cycle['End'], backward)
         timeDrivenForward = countTimeDriven(cycle['Begin'], cycle['End'], forward)
-        # Todo print in csv an repeat for forward
-        print(f'timeDrivenBackward: {timeDrivenBackward} timeDrivenForward: {timeDrivenForward}')
+        df = df.append({'timeDrivenBackward': timeDrivenBackward, 'timeDrivenForward': timeDrivenForward}, ignore_index=True)
+    return df
+
+
+def calculateAverageAndWriteFiles(df):
+    df.to_csv('data/output/DriveTimePerCycle.csv', sep=',', index=False)
+    averagePerMonth = df.mean()
+    dict = {'AveragePerMonthBackwards': averagePerMonth['timeDrivenBackward'],
+            'AveragePerMonthForward': averagePerMonth['timeDrivenForward']}
+    df2 = pd.DataFrame(dict, index=[1])
+    df2.to_csv('data/output/AveragePerMonth.csv', sep=',', index=False)
+    # Todo pro Schicht / Tag / Stunde
+    # Todo Zeit pro Cycle ab dem gefahren wird (wenn dort ein Zusammenhang besteht)
 
 
 def main():
-    # import data
     cycles = getCycles()
     backward = getSensorBackward()
     forward = getSensorForward()
-    analyzeCycles(cycles, backward, forward)
+    df = analyzeCycles(cycles, backward, forward)
+    calculateAverageAndWriteFiles(df)
 
 
 main()
